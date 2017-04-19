f03482618e73fa03b5adf55443c7a9ccf50e8ea1
lovelypig5 <lovelypig5@163.com>
2017-04-19 17:30:31 +0800

    add mobile

---
+++ mobile/ios/js/app.js
@@ -0,0 +1,13 @@
+import React, { AppRegistry } from 'react-native';
+import Router from './router';
+import styles from './styles';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router />
+            );
+    }
+}
+
+AppRegistry.registerComponent('wms', () => App);
