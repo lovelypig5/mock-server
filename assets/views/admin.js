8530e1c26c873227bf92705d72166d1dc5bf474a
lovelypig5 <lovelypig5@163.com>
2017-04-26 21:19:24 +0800

    change css load

--- assets/views/admin.js
+++ assets/views/admin.js
@@ -1,8 +1,5 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import 'less/admin.less';
-import '../lib/plugins/jsoneditor.min.css';
+import "bootstrap";
 import App from 'js/app.vue';
 
 if (process.env.NODE_ENV == "development") {
--- assets/views/index.js
+++ assets/views/index.js
@@ -1,8 +1,4 @@
 import 'bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
-import 'animate.css';
-import 'font-awesome/css/font-awesome.min.css';
 import 'less/index.less';
 
 $(window).load(() => {
