bdbb8ea9ea111ba5dd025adc1cde1e783269f529
lovelypig5 <lovelypig5@163.com>
2017-04-29 09:58:56 +0800

    change home page

--- assets/views/index.js
+++ assets/views/index.js
@@ -11,3 +11,24 @@ new WOW().init();
 $('.navbar-collapse a').click(() => {
     $(".navbar-collapse").collapse('hide');
 });
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+}
+
+$('.sendMsg').on('click', function(e) {
+    var c_name = $('#c_name').val();
+    var c_email = $('#c_email').val();
+    var c_message = $('#c_message ').val();
+    var response = $('.ajax-response');
+
+    if ((c_name == '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email))) {
+        response.fadeIn(500);
+        response.html('<i class="fa fa-warning"></i> 请填写相关的信息后再提交.');
+    } else {
+        response.html('');
+        var subject = "来自客户(" + c_name + ':' + c_email + ")的需求:" + c_message;
+        location.href = "mailto:lovelypig5@163.com?subject=" + subject;
+    }
+});
