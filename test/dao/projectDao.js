d35dcf2855af329f6e91449b808c103e7e53fa68
lovelypig5 <lovelypig5@163.com>
2017-05-01 22:38:29 +0800

    add unit test

--- test/dao/projectDao.js
+++ test/dao/projectDao.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect,
+    _ = require('lodash'),
     config = require('../../src/config'),
     userDao = require(`../../src/dao/${config.DB.dialect}/userDao`),
     projectDao = require(`../../src/dao/${config.DB.dialect}/projectDao`),
@@ -17,13 +18,85 @@ describe('ProjectDao', () => {
     })
 
     describe('#createProject()', () => {
-        it('create project with no empty', (done) => {
+        it('create empty project should be error', (done) => {
             var project = {};
-            projectDao.createProject(project).then(null,
-                (err) => {
-                    expect(err).to.not.be.ok;
-                    done();
-                }).catch((err) => {
+            projectDao.createProject(project).then(null, (err) => {
+                expect(err).to.have.any.keys('errors');
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('create project without name should be error', (done) => {
+            var project = {
+                name: '123',
+                desc: '',
+                beginPath: '/tt',
+                proxy: 'http://www.baidu.com',
+                isPublic: 1,
+                createTime: Date.now(),
+                modifyTime: Date.now(),
+                userId: user.id
+            };
+            projectDao.createProject(_.omit(project, ['name'])).then(null, ({ errors }) => {
+                expect(errors['name'].message).to.equal('Path `name` is required.');
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('create project without beginPath should be error', (done) => {
+            var project = {
+                name: '123',
+                desc: '',
+                beginPath: '/tt',
+                proxy: 'http://www.baidu.com',
+                isPublic: 1,
+                createTime: Date.now(),
+                modifyTime: Date.now(),
+                userId: user.id
+            };
+            projectDao.createProject(_.omit(project, ['beginPath'])).then(null, ({ errors }) => {
+                expect(errors['beginPath'].message).to.equal(
+                    'Path `beginPath` is required.');
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('create project without proxy should be error', (done) => {
+            var project = {
+                name: '123',
+                desc: '',
+                beginPath: '/tt',
+                proxy: 'http://www.baidu.com',
+                isPublic: 1,
+                createTime: Date.now(),
+                modifyTime: Date.now(),
+                userId: user.id
+            };
+            projectDao.createProject(_.omit(project, ['proxy'])).then(null, ({ errors }) => {
+                expect(errors['proxy'].message).to.equal('Path `proxy` is required.');
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('create project without userId should be error', (done) => {
+            var project = {
+                name: '123',
+                desc: '',
+                beginPath: '/tt',
+                proxy: 'http://www.baidu.com',
+                isPublic: 1,
+                createTime: Date.now(),
+                modifyTime: Date.now(),
+                userId: user.id
+            };
+            projectDao.createProject(_.omit(project, ['userId'])).then(null, ({ errors }) => {
+                expect(errors['userId'].message).to.equal('Path `userId` is required.');
+                done();
+            }).catch((err) => {
                 done(err);
             });
         });
