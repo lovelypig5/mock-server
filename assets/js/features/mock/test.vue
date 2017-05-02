0a7f1080229ab0c16262474e79c1491e9859bf1a
lovelypig5 <lovelypig5@163.com>
2017-05-02 17:42:42 +0800

    change user.id to user._id

--- assets/js/features/mock/test.vue
+++ assets/js/features/mock/test.vue
@@ -33,7 +33,7 @@ const MockTest = Vue.extend({
             return this.mockapi.type != 'GET';
         },
         token() {
-            return this.$store.state.user.id;
+            return this.$store.state.user._id;
         }
     },
     methods: {
