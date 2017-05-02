f8e8d80405e36d3836d46806b557b6f57be6b812
lovelypig5 <lovelypig5@163.com>
2017-05-02 16:57:39 +0800

    userDao test

--- test/dao/index.js
+++ test/dao/index.js
@@ -1,5 +1,114 @@
 var deps = ['./userDao', './projectDao'];
-var ret = [];
-deps.forEach((dep) => {
-    Array.prototype.push.apply(ret, require(dep));
-});
+var expect = require('chai').expect,
+    _ = require('lodash'),
+    config = require('../../src/config'),
+    userDao = require(`../../src/dao/${config.DB.dialect}/userDao`),
+    Errors = require('../../src/error');
+
+describe('User Register', () => {
+    var registerUser = {
+        name: 'ci_test',
+        password: 'ci_test'
+    }
+
+    it('register with empty object', (done) => {
+        var user = {};
+        userDao.register(user).then(null, (err) => {
+            expect(err).to.be.instanceof(TypeError);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('register with no name', (done) => {
+        var user = {
+            password: '123'
+        };
+        userDao.register(user).then(null, ({ errors }) => {
+            expect(errors['name'].message).to.equal(
+                'Path `name` is required.');
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('register with no password', (done) => {
+        var user = {
+            name: 'test'
+        };
+        userDao.register(user).then(null, (err) => {
+            expect(err).to.be.instanceof(TypeError);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('register with not a string password', (done) => {
+        var user = {
+            name: 'test',
+            password: 123
+        };
+        userDao.register(user).then(null, (err) => {
+            expect(err).to.be.instanceof(TypeError);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('register a user', (done) => {
+        userDao.register(registerUser).then((user) => {
+            registerUser = Object.assign(registerUser, user.toJSON());
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('remove a empty user', (done) => {
+        userDao.remove({}).then(null, (err) => {
+            expect(err).to.be.instanceof(Errors.UnknownError);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('remove a user', (done) => {
+        userDao.remove(registerUser).then((user) => {
+            registerUser = _.omit(registerUser, ['_id', 'salt']);
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    describe("Backend of Mock Server", () => {
+        before((done) => {
+            userDao.register(registerUser).then((user) => {
+                registerUser = Object.assign(registerUser, user.toJSON());
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        })
+
+        deps.forEach((dep) => {
+            var depends = require(dep);
+            depends(registerUser);
+        });
+
+        after((done) => {
+            userDao.remove(registerUser).then((user) => {
+                registerUser = _.omit(registerUser, ['_id', 'salt']);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        })
+    })
+
+})
--- test/dao/userDao.js
+++ test/dao/userDao.js
@@ -3,61 +3,56 @@ var expect = require('chai').expect,
     userDao = require(`../../src/dao/${config.DB.dialect}/userDao`),
     Errors = require('../../src/error');
 
-describe('UserDao', () => {
-    describe('#login()', () => {
-        it('login with empty object', (done) => {
-            var user = {};
-            userDao.login(user).then(null, (err) => {
-                expect(err).to.be.instanceof(
-                    Errors.AuthenticateFail
-                );
-                done();
-            }).catch((err) => {
-                done(err);
+var UserDaoTest = (registerUser) => {
+
+    describe('Test Functions in UserDao', () => {
+        describe('#login()', () => {
+            it('login with empty object', (done) => {
+                var user = {};
+                userDao.login(user).then(null, (err) => {
+                    expect(err).to.be.instanceof(TypeError);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
             });
-        });
-        it('login with empty userName', (done) => {
-            var user = { userName: '' };
-            userDao.login(user).then(null, (err) => {
-                expect(err).to.be.instanceof(
-                    Errors.AuthenticateFail
-                );
-                done();
-            }).catch((err) => {
-                done(err);
+            it('login with empty userName', (done) => {
+                var user = { userName: '' };
+                userDao.login(user).then(null, (err) => {
+                    expect(err).to.be.instanceof(TypeError);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
             });
-        });
-        it('login with empty password', (done) => {
-            var user = { password: '' };
-            userDao.login(user).then(null, (err) => {
-                expect(err).to.be.instanceof(
-                    Errors.AuthenticateFail
-                );
-                done();
-            }).catch((err) => {
-                done(err);
+            it('login with empty password', (done) => {
+                var user = { password: '' };
+                userDao.login(user).then(null, (err) => {
+                    expect(err).to.be.instanceof(Errors.AuthenticateFail);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
             });
-        });
-        it('login with sql inject parameters', (done) => {
-            var user = { userName: 'test', password: 'a or 1=1' };
-            userDao.login(user).then(null, (err) => {
-                expect(err).to.be.instanceof(
-                    Errors.AuthenticateFail
-                );
-                done();
-            }).catch((err) => {
-                done(err);
+            it('login with sql inject parameters', (done) => {
+                var user = { userName: 'test', password: 'a or 1=1' };
+                userDao.login(user).then(null, (err) => {
+                    expect(err).to.be.instanceof(Errors.AuthenticateFail);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
             });
-        });
-        it('login with given user', (done) => {
-            var user = { userName: 'test', password: 'test' };
-            userDao.login(user).then((result) => {
-                expect(result).to.have.all.keys(
-                    'id', 'name');
-                done();
-            }).catch((err) => {
-                done(err);
+            it('login with given user', (done) => {
+                userDao.login(registerUser).then((result) => {
+                    expect(result.toJSON()).to.have.all.keys('_id', 'name', 'salt');
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
             });
         });
     });
-});
+}
+
+module.exports = UserDaoTest;
