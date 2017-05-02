9a447b13fd8d3244d03b817ff48617fdd6e00602
lovelypig5 <lovelypig5@163.com>
2017-05-02 21:43:05 +0800

    fix bug

--- src/dao/mongo/projectDao.js
+++ src/dao/mongo/projectDao.js
@@ -22,29 +22,27 @@ class ProjectDao extends BaseDao {
             conditions._id = id;
         }
 
-        return await this.Entity.find(conditions, {userId: false}).exec();
+        return await this.Entity.find(conditions, { userId: false }).exec();
     }
 
     async modifyProject(id, userId, project) {
         var docs = await this.Entity.update({
             _id: id,
             userId: userId
-        }, {$set: project});
+        }, { $set: project });
         if (docs.n == 1) {
             return '修改项目详情成功';
         } else {
-            logger.error(`项目修改失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
-            return new Errors.UnknownError(`项目修改失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
+            throw new Errors.UnknownError(`项目修改失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
         }
     }
 
     async deleteProject(id, userId) {
-        var docs = await this.Entity.remove({_id: id, userId: userId});
+        var docs = await this.Entity.remove({ _id: id, userId: userId });
         if (docs.result.n == 1) {
             return "删除项目成功";
         } else {
-            logger.error(`项目删除失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
-            return new Errors.UnknownError(`项目删除失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
+            throw new Errors.UnknownError(`项目删除失败，未知原因，请联系管理员。项目ID: ${id}, 用户ID: ${userId}`);
         }
     }
 
