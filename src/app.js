4da4ac5ba61db526b2eb5953e2c5b111864b3d7d
lovelypig5 <lovelypig5@163.com>
2017-05-18 11:30:29 +0800

    remove cookie session, add access token

--- src/app.js
+++ src/app.js
@@ -1,37 +1,39 @@
-var express = require('express'),
-    path = require('path'),
-    filters = require('./filters'),
-    routes = require('./route'),
-    apis = require('./api'),
-    logger = require('./logger'),
-    consolidate = require('consolidate'),
-    mock = require('./service/mock');
+var express = require( 'express' ),
+    path = require( 'path' ),
+    filters = require( './filters' ),
+    routes = require( './route' ),
+    apis = require( './api' ),
+    logger = require( './logger' ),
+    consolidate = require( 'consolidate' ),
+    mock = require( './service/mock' );
 
 var app = express();
-app.engine('html', consolidate.ejs);
-app.set('views', '../assets/dist');
-app.set('view engine', 'html');
-app.set('x-powered-by', false);
+app.engine( 'html', consolidate.ejs );
+app.set( 'views', '../assets/dist' );
+app.set( 'view engine', 'html' );
+app.set( 'x-powered-by', false );
 
-filters.forEach((filter) => {
-    if (filter.route) {
-        app.all(filter.route, filter.filter);
+filters.forEach( ( filter ) => {
+    if ( filter.route ) {
+        app.all( filter.route, filter.filter );
     } else {
-        app.use(filter.filter);
+        app.use( filter.filter );
     }
-});
-apis.forEach((api) => {
+} );
+
+apis.forEach( ( api ) => {
     var method = api.method || 'get';
-    app[method](api.route, api.func);
-});
-routes.forEach((route) => {
-    app.use(route.route, route.router);
-});
+    app[ method ]( api.route, api.func );
+} );
 
-app.use(express.static(path.resolve('../assets/dist')));
+routes.forEach( ( route ) => {
+    app.use( route.route, route.router );
+} );
 
-mock.init().then(() => {
-    app.listen(3003, () => {
-        logger.info(`Backend service listening on port 3003!`);
-    });
-});
+app.use( express.static( path.resolve( '../assets/dist' ) ) );
+
+mock.init().then( () => {
+    app.listen( 3003, () => {
+        logger.info( `Backend service listening on port 3003!` );
+    } );
+} );
