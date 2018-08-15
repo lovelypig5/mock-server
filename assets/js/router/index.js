eaa0ec8401a1e34a15440a1cff051b84c378df3e
lovelypig5 <lovelypig5@163.com>
2018-08-15 10:24:08 +0800

    support oauth2

--- assets/js/router/index.js
+++ assets/js/router/index.js
@@ -1,31 +1,31 @@
-import Router from 'vue-router';
-Vue.use(Router);
+import Router from "vue-router";
+Vue.use( Router );
 
-var routes = [{
-    path: '/',
-    component(resolve) {
-        return require(['../features/login.vue'], resolve);
+var routes = [ {
+    path: "/",
+    component( resolve ) {
+        return require( [ "../features/project/list.vue" ], resolve );
     }
 }, {
-    path: '/project',
-    component(resolve) {
-        return require(['../features/project/list.vue'], resolve);
+    path: "/project",
+    component( resolve ) {
+        return require( [ "../features/project/list.vue" ], resolve );
     }
 }, {
-    path: '/:id',
-    name: 'mockSet',
-    component(resolve) {
-        return require(['../features/mock/index.vue'], resolve);
+    path: "/:id",
+    name: "mockSet",
+    component( resolve ) {
+        return require( [ "../features/mock/index.vue" ], resolve );
     },
     subRoutes: {
-        "/:name": function (params) {
-            this.vue.$broadcast("changeMenuBy", params.name);
+        "/:name": function( params ) {
+            this.vue.$broadcast( "changeMenuBy", params.name );
         }
     }
-}];
+} ];
 
-var router = new Router({
+var router = new Router( {
     routes
-});
+} );
 
 export default router;
