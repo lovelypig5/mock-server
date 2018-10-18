1ba83b4b3a10f04e8ad1e3e1b41dbafacf9f9500
lovelypig5 <lovelypig5@163.com>
2018-10-18 15:20:52 +0800

    change logger

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,52 @@
-var logger = require('log4js').getLogger('[mock-server]');
-logger.setLevel('DEBUG');
+const log4js = require( "log4js" );
+log4js.configure( {
+    pm2: true,
+    appenders: {
+        console: {
+            type: "console"
+        },
+        out: {
+            type: "stdout",
+            layout: {
+                type: "pattern",
+                pattern: "%[[%x{reqid}] [%d] [%p] %x{type} [%x{user}]%] %m%n",
+                tokens: {
+                    reqid( logEvent ) {
+                        if ( logEvent.context.reqid ) {
+                            return `${logEvent.context.reqid}`;
+                        }
+                        return "";
+                    },
+                    type( logEvent ) {
+                        if ( logEvent.context.type ) {
+                            return `[${logEvent.context.type}]`;
+                        }
+                        return logEvent.categoryName;
+                    }
+                }
+            }
+        },
+        file: {
+            type: "file",
+            filename: "logs/access.log",
+            maxLogSize: 10485760,
+            layout: {
+                type: "messagePassThrough"
+            },
+            compress: true
+        }
+    },
+    categories: {
+        "[Logger]": {
+            appenders: [ "out" ],
+            level: "debug"
+        },
+        default: {
+            appenders: [ "file" ],
+            level: "info"
+        }
+    }
+} );
+var logger = log4js.getLogger( "[Logger]" );
 
 module.exports = logger;
